f825bde90733af5a09c91521859cbf4a92f65c44
Raphael Isla <rpbisla0025@gmail.com>
2023-05-12 14:43:59 +0800

    close

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {ImageBackground, Text, View, StyleSheet, Image, TextInput, TouchableOpacity} from 'react-native';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import OnBoarding from './screens/Onboarding';
+import SignIn from './screens/SignIn';
+import Dashboard from './screens/Dashboard';
+import Profile from './screens/Profile';
+
+const Stack = createNativeStackNavigator(); 
+const TabNav = createBottomTabNavigator();
+
+function DashScreen(){
+  return (
+    <TabNav.Navigator initialRouteName='Dashboard' screenOptions={{headerShown: false,showLabel: false, tabBarStyle:{
+        position: 'absolute',
+        bottom: 0,
+        left: 0, 
+        right: 0,
+        elevation: 5,
+        height: 70,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        backgroundColor: '#fff'
+      }
+    }} >
+        <TabNav.Screen name='Dashboard' component={Dashboard} options={{title: 'Dashboard'}}/>
+        <TabNav.Screen name='Profile' component={Profile} options={{title: 'Profile'}}/>
+    </TabNav.Navigator>
+  ); 
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false, }}>
+          <Stack.Screen name="OnBoarding" component={OnBoarding}/>
+          <Stack.Screen name="SignIn" component={SignIn}/>  
+          <Stack.Screen name="DashScreen" component={DashScreen}/>  
+        </Stack.Navigator>
+      </NavigationContainer>
+  ); 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  barStyle: {
+    position: 'absolute',
+    bottom: 25,
+    left: 20, 
+    right: 20,
+    elevation: 0,
+    height: 90,
+    borderRadius: 15,
   },
 });
